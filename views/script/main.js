d965c0e603a1d481c8b23447f99def727f8576cc
Sacha Bron <sacha.bron@copperflake.com>
2016-11-19 16:17:49 +0100

    Add client-side WS

--- views/script/main.js
+++ views/script/main.js
@@ -3,5 +3,32 @@ window.onload = function () {
 }
 
 function onPageLoad() {
-	
+	initWS();
 };
+
+function initWS() {
+	var ws = new WebSocket(document.location.origin.replace(/^http/, "ws") + "/ws");
+
+	ws._send = ws.send;
+	ws.send = function (obj) {
+		ws._send(angular.toJson(obj));
+	}
+
+	ws.onopen = function (event) {
+		console.log("WebSocket open");
+		ws.send({ type: "ping", data: { message: "echo" } });
+	}
+
+	ws.onmessage = function (event) {
+		var data = JSON.parse(event.data);
+		console.log(data);
+	}
+
+	ws.onclose = function (event) {
+		console.warn("WebSocket closed:", event);
+	}
+
+	ws.onerror = function (event) {
+		console.error("WebSocket error:", event);
+	}
+}
