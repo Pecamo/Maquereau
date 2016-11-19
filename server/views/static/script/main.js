ab1bd0c32ca052afed8a0947c8d3a470b0767322
Sacha Bron <sacha.bron@copperflake.com>
2016-11-19 23:36:40 +0100

    Change icon size

--- server/views/static/script/main.js
+++ server/views/static/script/main.js
@@ -65,7 +65,6 @@ function initWS() {
 				queryLayout(message.data.name);
 				break;
 			case 'layout':
-				console.log('New layout:', JSON.stringify(message.data));
 				vm.tiles = message.data.tiles;
 				break;
 			default:
