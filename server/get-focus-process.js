a79d1675a775b17477039b66d3a472b22799f6f8
Kewin Dousse <me@protectator.ch>
2016-11-20 01:38:55 +0100

    Fixing Windows compatibility and adding final WebStorm icons

--- server/get-focus-process.js
+++ server/get-focus-process.js
@@ -2,13 +2,19 @@
 
 const exec = require('child_process').exec;
 
+const regexPattern = new RegExp(/(?:[\d\.]+\s+){7}((?:\s?\w+)+)/);
+
 function getFocusProcess (cb) {
 	if (process.platform.toLowerCase().includes('win')) {
-		let command = 'C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe . "./windowsproc.ps1";'
+		let command = 'C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe . "./windowsproc.ps1";';
 		let proc = exec(command);
 
 		proc.stdout.on('data', (data) => {
-			cb(data.trim());
+			var found = data.match(regexPattern);
+			if (found) {
+				console.log(found[1].trim());
+				cb(found[1].trim());
+			}
 		});
 
 		proc.stderr.on('data', (data) => {
